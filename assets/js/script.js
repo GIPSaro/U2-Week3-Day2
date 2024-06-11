18bc4a810fe054047420fb0f67b32cbe6898d23d
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2024-06-11 16:38:52 +0200

    commit2

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,65 @@ function saveName() {
   }
 }
 
+function createRemoveButton() {
+  const registratioForm = document.getElementById("formRegist");
+
+  if (!document.getElementById("removeButtom")) {
+    const removeButtom = document.createElement("button");
+    removeButtom.textContent = "Rimuovi Nome";
+    removeButtom.setAttribute("id", "removeButtom");
+    removeButtom.addEventListener("click", removeName);
+    registratioForm.appendChild(removeButtom);
+  }
+}
+function updateElapsedTime() {
+  const timeElapsed = document.getElementById("timeElapsed");
+  let seconds = parseInt(sessionStorage.getItem("elapsedSeconds") || 0, 10);
+
+  setInterval(function () {
+    seconds++;
+    sessionStorage.setItem("elapsedSeconds", seconds);
+    timeElapsed.textContent = seconds;
+  }, 1000);
+}
+
+function createRegistrationForm() {
+  const registratioForm = document.getElementById("formRegist");
+
+  const nameLabel = document.createElement("label");
+  nameLabel.setAttribute("for", "name");
+  nameLabel.textContent = "Nome:";
+  registratioForm.appendChild(nameLabel);
+
+  const nameInput = document.createElement("input");
+  nameInput.setAttribute("type", "text");
+  nameInput.setAttribute("id", "name");
+  nameInput.placeholder = "Inserisci il tuo nome";
+  registratioForm.appendChild(nameInput);
+
+  const saveButtom = document.createElement("button");
+  saveButtom.textContent = "Salva Nome";
+  saveButtom.addEventListener("click", saveName);
+  registratioForm.appendChild(saveButtom);
+
+  const removeButtom = document.createElement("button");
+  removeButtom.textContent = "Rimuovi Nome";
+  removeButtom.addEventListener("click", removeName);
+  registratioForm.appendChild(removeButtom);
+}
 function removeName() {
   localStorage.removeItem("userName");
   document.getElementById("displayName").textContent = "";
   const nameInput = document.getElementById("name");
   nameInput.value = "";
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const savedName = localStorage.getItem("userName");
+  if (savedName) {
+    document.getElementById("displayName").textContent = savedName;
+    createRemoveButton();
+  }
+  createRegistrationForm();
+  updateElapsedTime();
+});
